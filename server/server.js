825ffb8454adf7b02d3fcc971731e22ee26c5e83
ISAAC BENEDICT <isaacbenedict001@gmail.com>
2025-07-12 07:06:25 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,13 +33,11 @@ app.use(express.json());
 // Enable CORS
 app.use(cors({
   origin: [
-    'https://mern-blog-5h98.onrender.com', // your deployed frontend
+    'https://mern-blog-5h98.onrender.com',
     'http://localhost:3000',
     'http://localhost:5173'
   ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }));
 
 // Security headers
